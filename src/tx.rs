9cfe1bca639820e3aa535a98eb8854fca9711906
David Ireland <davidji@pobox.com>
2020-12-21 14:16:04 +0000

    Correct clear MAX_RT

--- src/tx.rs
+++ src/tx.rs
@@ -71,11 +71,7 @@ impl<D: Device> TxMode<D> {
 
             // TX won't continue while MAX_RT is set
             if status.max_rt() {
-                let mut clear = Status(0);
-                // Clear TX interrupts
-                clear.set_tx_ds(true);
-                clear.set_max_rt(true);
-                self.device.write_register(clear)?;
+                self.flush_tx()?;
             }
         }
         // Can save power now
